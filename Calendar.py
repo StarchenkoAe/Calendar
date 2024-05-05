085f8b635435a8ccaf80b0dd4f1ecac8bccf7489
StarchenkovAE <lacherov@gmail.com>
2024-05-05 12:44:11 +0400

    Импортируем нужные библиотеки.

---
+++ Calendar.py
@@ -0,0 +1,5 @@
+#Импортируем нужные библиотеки
+import tkinter as tk   #Для создания пользовательского интерфейса
+from tkinter import ttk
+from datetime import date, datetime  #Для работы с временем
+import calendar #Для работы с функциями календаря
